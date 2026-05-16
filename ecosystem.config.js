cbf22249a9bc8662b6bb8da42d94fe630f9ecbf2
Fujimiya Shion <tranthangloi.dev@gmail.com>
2026-05-16 17:37:58 +0700

    feat: update ecosystem file to run as cluster mode

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,3 +1,7 @@
+require('dotenv').config({ path: '/var/www/html/shining-english/.env' })
+
+const workers = Number.parseInt(process.env.PM2_WORKERS || '1', 10)
+
 module.exports = {
   apps: [
     {
@@ -5,6 +9,11 @@ module.exports = {
       script: 'node_modules/next/dist/bin/next',
       args: 'start -p 4000',
       cwd: '/var/www/html/shining-english',
+
+      // Cluster mode
+      exec_mode: 'cluster',
+      instances: Number.isFinite(workers) && workers > 0 ? workers : 1,
+
       env: {
         NODE_ENV: 'production',
       },
